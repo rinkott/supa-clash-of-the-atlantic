0cd1541f3c1c4707da0cff3e16c6cb782c092733
Rin <hello@rin.cm>
2023-10-14 19:11:58 +0300

    fix: spelling

--- src/server/routers/casters.ts
+++ src/server/routers/casters.ts
@@ -21,7 +21,7 @@ const currentlySpeaking = new Map<string, VoiceActivity>()
 
 const currentCasters = new Map<string, Caster>([
 	['123', { name: 'Player_no_Name', discordId: '123' }],
-	['456', { name: 'Isxen', discordId: '456' }],
+	['456', { name: 'Ixsen', discordId: '456' }],
 ])
 
 export const casterRouter = router({

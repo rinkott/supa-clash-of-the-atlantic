f976afb16178df92519e5ed9349bcaf7a84a4e37
Rin <hello@rin.cm>
2023-10-14 17:42:51 +0300

    WIP(signups): add NA players

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
 import { trpc } from '../utils/trpc'
 
 export default function IndexPage() {
-	const result = trpc.healthcheck.useQuery()
-
-	if (!result.data) {
-		return (
-			<div>
-				<h1>Loading...</h1>
-			</div>
-		)
-	}
 	return (
 		<div>
-			<h1>{result.data}</h1>
+			<h1>haiii :3333</h1>
 		</div>
 	)
 }

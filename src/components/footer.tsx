312b4d9b76ddbab792b7ae459c892788dab484c0
Rin <hello@rin.cm>
2023-10-15 17:18:40 +0300

    feat(footer): add rankBy misses for challenge match

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -44,16 +44,21 @@ const TeamScores = (props: {
 	rightTeamScore: number
 	leftTeamAccuracy: number
 	rightTeamAccuracy: number
+
+	leftTeamMisses: number
+	rightTeamMisses: number
+
+	rankBy?: 'misses' | 'accuracy'
 }) => (
-	<div className="flex gap-[40px]">
-		<div className="flex flex-col text-right">
-			<h1 className="font-bold text-1xl text-red-500 -mb-2">
-				NA
-			</h1>
+	<div className="flex gap-[40px] pb-4 justify-center">
+		<div className="flex flex-col text-right w-[150px]">
+			<h1 className="font-bold text-1xl text-red-500 -mb-2">NA</h1>
 			<h1 className="font-bold text-4xl -mb-1">
-				{(props.leftTeamAccuracy * 100).toFixed(2)}%
+				{props.rankBy !== 'misses'
+					? `${(props.leftTeamAccuracy * 100).toFixed(2)}%`
+					: `${props.leftTeamMisses}x` || 0}
 			</h1>
-			<h2 className="font-bold text-[20px]">
+			<h2 className="font-bold text-[20px] inline-block w-full">
 				<NumberAnimation
 					value={props.leftTeamScore}
 					formatter={localeFormatter}
@@ -61,14 +66,14 @@ const TeamScores = (props: {
 				/>
 			</h2>
 		</div>
-		<div className="flex flex-col text-left">
-			<h1 className="font-bold text-1xl text-blue-500 -mb-2">
-				EU
-			</h1>
+		<div className="flex flex-col text-left w-[150px]">
+			<h1 className="font-bold text-1xl text-blue-500 -mb-2">EU</h1>
 			<h1 className="font-bold text-4xl -mb-1">
-				{(props.rightTeamAccuracy * 100).toFixed(2)}%
+				{props.rankBy !== 'misses'
+					? `${(props.rightTeamAccuracy * 100).toFixed(2)}%`
+					: `${props.rightTeamMisses}x` || 0}
 			</h1>
-			<h2 className="font-bold text-[20px]">
+			<h2 className="font-bold text-[20px] inline-block w-full">
 				<NumberAnimation
 					value={props.rightTeamScore}
 					formatter={localeFormatter}
@@ -77,9 +82,21 @@ const TeamScores = (props: {
 			</h2>
 		</div>
 
-		<div className='absolute opacity-80 -bottom-5 left-1/2 transform -translate-x-1/2 font-bold text-sm whitespace-nowrap'>
-			{Math.abs(props.leftTeamScore - props.rightTeamScore)} ({(Math.abs(props.leftTeamAccuracy - props.rightTeamAccuracy) * 100).toFixed(2)}%)
-		</div>
+		{props.rankBy === 'misses' ? (
+			<>
+				<div className="absolute opacity-80 -bottom-1 left-1/2 transform -translate-x-1/2 font-bold text-sm whitespace-nowrap">
+					{Math.abs(props.leftTeamMisses - props.rightTeamMisses)}
+				</div>
+			</>
+		) : (
+			<div className="absolute opacity-80 -bottom-1 left-1/2 transform -translate-x-1/2 font-bold text-sm whitespace-nowrap">
+				{Math.abs(props.leftTeamScore - props.rightTeamScore)} (
+				{(
+					Math.abs(props.leftTeamAccuracy - props.rightTeamAccuracy) * 100
+				).toFixed(2)}
+				%)
+			</div>
+		)}
 	</div>
 )
 
@@ -90,16 +107,24 @@ function Footer(props: {
 	rightTeamScore: number
 	rightTeamAccuracy: number
 
-	mapHash: string,
+	leftTeamMisses: number
+	rightTeamMisses: number
+
+	mapHash: string
 	diff: Difficulty
+	mapProgress?: number
+	rankBy?: 'misses' | 'accuracy'
 }) {
 	return (
 		<>
 			<div className="w-full relative h-[133px] font-poppins text-white bg-footer-bar bg-cover bg-no-repeat z-30">
-				<TugOfWar leftTeamAccuracy={props.leftTeamAccuracy} rightTeamAccuracy={props.rightTeamAccuracy} />
+				<TugOfWar
+					leftTeamAccuracy={props.leftTeamAccuracy}
+					rightTeamAccuracy={props.rightTeamAccuracy}
+				/>
 
 				<div className="w-full h-full absolute inset-0 justify-between items-center px-9 flex">
-					<BeatMap hash={props.mapHash} diff={props.diff} />
+					<BeatMap hash={props.mapHash} diff={props.diff} mapProgress={props.mapProgress} />
 
 					<div className="absolute left-[50%] transform translate-x-[-50%]">
 						<TeamScores
@@ -107,6 +132,9 @@ function Footer(props: {
 							rightTeamScore={props.rightTeamScore}
 							leftTeamAccuracy={props.leftTeamAccuracy}
 							rightTeamAccuracy={props.rightTeamAccuracy}
+							leftTeamMisses={props.leftTeamMisses}
+							rightTeamMisses={props.rightTeamMisses}
+							rankBy={props.rankBy}
 						/>
 					</div>
 

8bad41764d2fbf0110b5564ff671c74a15a64aaa
Rin <hello@rin.cm>
2023-10-15 17:40:41 +0300

    feat(intermission): change countdown

--- src/pages/intermission.tsx
+++ src/pages/intermission.tsx
@@ -7,42 +7,38 @@ import type { VoiceActivity } from '~/server/routers/casters'
 import Header from '~/components/header'
 import AnimatedLogo from '~/components/animated-logo'
 
+import Countdown from 'react-countdown'
+
+const countdownRenderer = ({
+	hours,
+	minutes,
+	seconds,
+	completed,
+}: {
+	hours: number
+	minutes: number
+	seconds: number
+	completed: boolean
+}) => {
+	if (completed) {
+		return '00:00'
+	} else {
+		return (
+			<span>
+				{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
+			</span>
+		)
+	}
+}
+
 function Intermission() {
-	const [casters, setCasters] = useState<VoiceActivity[]>([])
-
-	const [countdown, setCountdown] = useState<number>()
-
-	const [seconds, setSeconds] = useState(0)
-
-	trpc.caster.subscribeToCasters.useSubscription(null, {
-		onData(data) {
-			setCasters(data)
-		},
-	
-		onStarted() {
-			console.log('Listening for caster activity')
-		},
-	})
+	const [countdown, setCountdown] = useState<number>(0)
 
 	trpc.countdown.subscribe.useSubscription(null, {
 		onData(data) {
 			setCountdown(data)
-			setSeconds(data)
 		},
 	})
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setSeconds((countdown) => countdown ? countdown - 1 : 0)
-		}, 1000)
-
-		return () => {
-			clearInterval(interval)
-		}
-	}, [countdown])
-
-	const fullMinutes = countdown ? Math.floor(seconds / 60) : 0
-	const timeLeft = countdown ? `${String(fullMinutes).padStart(2, '0')}:${String(seconds - fullMinutes  * 60).padStart(2, '0')}` : null
 
 	return (
 		<main className="w-screen h-screen bg-black flex items-center flex-col pt-32 text-white">
@@ -50,7 +46,12 @@ function Intermission() {
 				<Logo className="w-[946px]" />
 			</div>
 			<Header>
-				{timeLeft}
+				<Countdown
+					key={countdown}
+					renderer={countdownRenderer}
+					date={Date.now() + countdown * 1000}
+					autoStart
+				/>
 			</Header>
 			<footer className="w-full font-druk text-white text-2xl flex justify-between uppercase mt-auto p-8">
 				<div>
@@ -59,13 +60,13 @@ function Intermission() {
 					<span className="outline-text text-transparent">production</span>
 				</div>
 
-				<div>
+				{/* <div>
 					CASTED BY
 					&nbsp;
 					<span className="outline-text text-transparent">
 						{casters.map((va) => va.caster.name).join(', ')}
 					</span>
-				</div>
+				</div> */}
 			</footer>
 		</main>
 	)
